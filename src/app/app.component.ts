87d563592736cc1518d7c8913bf21135b17c6328
Gabriel Ramos <gabrielramoss232@gmail.com>
2024-11-12 16:32:23 -0300

    e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,25 @@ import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { UserComponent } from './components/user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./components/tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CabecalhoComponent,RodapeComponent,UserComponent],
+  imports: [CabecalhoComponent, RodapeComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS
+  selectUserId = 'ul';
+
+ get selectUser() {
+  return this.users.find((user) => user.id === this.selectUserId)!
+ }
+
+  onSelectUser(id: string) {
+  this.selectUserId = id;
+
+  }
 }
