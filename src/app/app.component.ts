30f08aa8fa221956a4b5d2a617862c1e7e4d7150
Gabriel Ramos <gabrielramoss232@gmail.com>
2024-11-22 14:47:02 -0300

    h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
-import { RodapeComponent } from './components/rodape/rodape.component';
 import { UserComponent } from './components/user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from "./components/tasks/tasks.component";
@@ -8,7 +7,7 @@ import { TasksComponent } from "./components/tasks/tasks.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CabecalhoComponent, RodapeComponent, UserComponent, TasksComponent],
+  imports: [CabecalhoComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 type User = {
   id: string;
@@ -26,4 +26,3 @@ get imagePath() {
   this.select.emit(this.user.id);
  }
 }
-
